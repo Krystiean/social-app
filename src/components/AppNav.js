861000302172794822656eef3aa1a3e67675cea7
Krystiean <krystiankesek56@gmail.com>
2024-10-24 23:25:01 +0200

    Dodanie logowania i wylogowywania

--- src/components/AppNav.js
+++ src/components/AppNav.js
@@ -1,19 +1,45 @@
+import axios from "axios";
 import "./AppNav.css"
 import { Link } from "react-router-dom";
 
-const AppNav = () => {
+const AppNav = (props) => {
+
+    const handleLogout = (e) => {
+        e.preventDefault();
+        axios
+			.post('https://akademia108.pl/api/social-app/user/logout', {
+				mode: 'cors',
+			})
+			.then((res) => {
+                if(res.data.message) {
+                    props.setUser(null);
+                    localStorage.setItem('user', null);
+                }
+            })
+			.catch((error) => {
+                props.setUser(null);
+                localStorage.setItem('user', null);
+				console.log(error);
+			});
+    }
+        
+
     return (
         <nav className="mainNav">
             <ul>
                 <li>
                     <Link to="/">Home</Link>
                 </li>
-                <li>
+                {!props.user && <li>
                     <Link to="/login">Login</Link>
-                </li>
-                <li>
+                </li>}
+                {!props.user && <li>
                     <Link to="/signup">signup</Link>
-                </li>
+                </li>}
+
+                {props.user && <li>
+                    <Link to="/" onClick={handleLogout}>Logout</Link>
+                </li>}
             </ul>
         </nav>
     )
